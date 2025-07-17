664d23a31ec59e6217b93347bf28f9adc8a89dd9
MugishaJuniorFred <148188347+MugishaJuniorFred@users.noreply.github.com>
2025-07-17 05:01:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+/* Typing Text Code */
+
+var sliderCounter = 0;
+var sliderContent = [
+	"Developer",
+	"Designer",
+	"Youtube",
+	"Code",
+];
+
+var sliderValue = document.querySelector("#SliderValue");
+
+function slide() {
+	if (sliderCounter >= sliderContent.length) {
+		sliderCounter = 0;
+	}
+	sliderValue.innerHTML = "";
+	sliderValue.classList.remove("holder-animation")
+	void sliderValue.offsetWidth;
+	sliderValue.classList.add("holder-animation")
+
+	for (i = 0; i < sliderContent[sliderCounter].length; i++) {
+		let letterDiv = document.createElement("div");
+		letterDiv.innerHTML = sliderContent[sliderCounter][i];
+
+		if (letterDiv.innerHTML == "") {
+			letterDiv.innerHTML = "&nbsp;";
+		}
+		letterDiv.classList.add("start")
+		letterDiv.classList.add("animation")
+		letterDiv.style.animationDelay = i / 10 + "s";
+		sliderValue.appendChild(letterDiv);
+	}  
+	sliderCounter++;
+	  
+}
+
+slide();
+setInterval(slide, 2000);
+
+/* toggle menu */
+
+$('.menu-btn').click(function(){
+	$('.menu').toggleClass("active");
+	$('.menu-btn i').toggleClass("active");
+})
+
+/* Scroll Animation */
+const sr = scrollReveal({
+	origin: "top",
+	disance: "80px",
+	duration: 2000,
+	reset: true
+
+})
+
+sr.reveal(".featured-text" ,{});
+sr.reveal(".home-img", {delay: 200 });
